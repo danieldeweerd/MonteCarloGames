e536829d7645ab1ad02aa4fc27a99ab92f0c57d0
Daniël <danieldeweerd@hotmail.nl>
2019-12-22 00:51:02 +0100

    Added basic graphing functionality

--- src/MonteCarloSimulator.java
+++ src/MonteCarloSimulator.java
@@ -1,34 +1,75 @@
 import cardgamelib.DrinkingGame;
+
 import games.LinearBus;
+import org.knowm.xchart.*;
+import org.knowm.xchart.style.Styler;
+
+
+import java.util.*;
 
 public class MonteCarloSimulator {
 
-    private static final int NUMBER_OF_TEST_RUNS = 20000;           // Number of times to simulate the game
+    private static final int NUMBER_OF_TEST_RUNS = 1000000;           // Number of times to simulate the game
+    static java.util.List<Integer> data = new ArrayList<Integer>();
+    static double[] frequencyTable = new double[100];
+    static double[] possibleValues = new double[100];
 
     /**
      * Simulate a drinking game a large amount of times to get
      * the average amount of taken sips
      *
-     * @param drinkingGame
+     * @param drinkingGame A game to simulate
      * @return sips
      */
 
     public static int simulate(DrinkingGame drinkingGame) {
         int totalAmountOfSips = 0;
+        long btime = System.currentTimeMillis();
         for (int i = 0; i < NUMBER_OF_TEST_RUNS; i++) {
             int slokken = drinkingGame.run();
+            //System.out.println(slokken);
+            data.add(slokken);
             totalAmountOfSips += slokken;
         }
+        long elapsed = System.currentTimeMillis() - btime;
+        System.out.println("Elapsed: " + elapsed);
+        System.out.println(Collections.frequency(data, 0));
+
+        for (int i = 0; i < frequencyTable.length; i++) {
+            frequencyTable[i] = Collections.frequency(data, i);
+        }
+
         return totalAmountOfSips / NUMBER_OF_TEST_RUNS;
     }
 
+
     public static void main(String[] args) {
-        int numberOfClosedCards = 3;
         int numberOfCardsOnTable = 8;
-        DrinkingGame bus = new LinearBus(numberOfCardsOnTable, numberOfClosedCards);
+        DrinkingGame bus = new LinearBus(numberOfCardsOnTable);
         int slokkenGemiddeld = simulate(bus);
-        System.out.println(numberOfCardsOnTable + " KAARTEN WAARVAN "
-                + numberOfClosedCards + " DICHT: " + slokkenGemiddeld);
+        System.out.println("AVERAGE: " + slokkenGemiddeld);
+        System.out.println("0 FREQ: " + Collections.frequency(data, 0));
+        System.out.println("10 FREQ: " + Collections.frequency(data, 10));
+
+        scatterPlot();
+
+    }
+
+    public static void scatterPlot()
+    {
+        // Create Chart
+        XYChart chart = new XYChartBuilder().width(800).height(600).build();
+
+        // Customize Chart
+        chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
+        chart.getStyler().setChartTitleVisible(false);
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideSW);
+        chart.getStyler().setMarkerSize(16);
+
+
+        chart.addSeries("Gaussian Blob", frequencyTable);
+
+        new SwingWrapper(chart).displayChart();
 
     }
 }
--- src/games/LinearBus.java
+++ src/games/LinearBus.java
@@ -5,61 +5,58 @@ import cardgamelib.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Subclass of DrinkingGame that simulates bussen
+ */
 public class LinearBus extends DrinkingGame {
-	CardStack stack = new CardStack();
-	List<Card> cardsOnTable = new ArrayList<Card>();
-	int numberOfCardsOnTable;
-	int numberOfClosedCards;
-	int counter = 0;
+    CardStack stack  = new CardStack();
+    List<Card> cardsOnTable = new ArrayList<Card>();    //	Represents cards on the table
+    int numberOfCardsOnTable;
+    int counter = 0;
 
-	public LinearBus(int numberOfCardsOnTable, int numberOfClosedCards) {
-		this.numberOfCardsOnTable = numberOfCardsOnTable;
-		this.numberOfClosedCards = numberOfClosedCards;
+    public LinearBus(int numberOfCardsOnTable) {
+        this.numberOfCardsOnTable = numberOfCardsOnTable;
 
-		for (int i = 0; i < numberOfCardsOnTable; i++)
-			cardsOnTable.add(stack.draw());
-		for (int j = 0; j < numberOfClosedCards; j++)
-			cardsOnTable.get(j).setClosed(true);
-	}
+        for (int i = 0; i < numberOfCardsOnTable; i++)
+            cardsOnTable.add(stack.draw());                        // Lay down initial set of cards
+    }
 
-	@Override
-	public String getName() {
-		return "Bussen";
-	}
+    @Override
+    public String getName() {
+        return "Bussen";
+    }
 
-	public int run() {
-		int slokken = 0;
-		while (counter != numberOfCardsOnTable) {
-			Card c1 = cardsOnTable.get(counter);
-			Card c2 = stack.draw();
-			if (c1.isClosed()) {
-				Comparison pred = CardGuesser.randomComp();
-				Comparison res = c2.compare(c1);
-				if (res == pred) {
-					counter++;
-					continue;
-				} else {
-					counter = 0;
-					slokken++;
-					continue;
-				}
-			} else {
-				Comparison pred = CardGuesser.predictOtherCard(c1);
-				Comparison res = c2.compare(c1);
-				if (res == pred) {
-					counter++;
-					continue;
-				} else {
-					counter = 0;
-					slokken++;
-					continue;
-				}
-			}
-		}
+    public int run() {
+        stack = new CardStack();
+        int slokken = 0;
+        counter = 0;
+        while (counter != numberOfCardsOnTable) {                       // While the end of the bus hasn't been reached...
+            Card c1 = cardsOnTable.get(counter);                        // Get the c
+            Card c2 = stack.draw();
 
-		counter = 0;
-		return slokken;
-	}
+
+            Comparison pred = CardGuesser.predictOtherCard(c1);
+            Comparison res = c2.compare(c1);
+
+//            System.out.println("--------------");
+//            System.out.println("Card on table " + c1);
+//            System.out.println("Prediction: " + pred);
+//            System.out.println("Drawn card: " + c2.toString());
+            boolean js = pred == res;
+//            System.out.println("Result: " + js);
+
+            if (res == pred) {
+                counter++;
+//                System.out.println("Verder");
+            } else {
+                counter = 0;
+//                System.out.println("SLOK!");
+                slokken++;
+            }
+
+        }
+        return slokken;
+    }
 
 
 }
